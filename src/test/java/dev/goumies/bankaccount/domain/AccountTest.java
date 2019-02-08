3ef75906ba9c8ac42271e3f645dfa53ce4b40280
Romy <contact@romyalula.com>
2019-02-08 19:06:33 +0100

    Initial commit

---
+++ src/test/java/dev/goumies/bankaccount/domain/AccountTest.java
@@ -0,0 +1,11 @@
+package dev.goumies.bankaccount.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class AccountTest {
+    @Test
+    public void name() {
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
